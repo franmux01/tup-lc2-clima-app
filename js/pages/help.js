6932d5da45042e37036ee71663d3e572c0213b4f
Francisco Mule <franciscomule2501@gmail.com>
2022-06-27 20:26:46 -0300

    TP Integrador 3. FINAL

--- js/pages/help.js
+++ js/pages/help.js
@@ -1,17 +1,33 @@
-document.getElementById('email').addEventListener('input', function(event) {
-    campo = event.target;
-    valido = document.getElementById('emailBad');
-        
-    emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+function removeMessage() { 
+    setTimeout(function() {
+        document.getElementsByClassName("alert-message")[0].remove();
+    }, 3000);
+}
+
+function validarEmail(email) {
+  
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
     
-    if (emailRegex.test(campo.value)) {
-        document.getElementById("emailBad").innerHTML+= 'valido';
-        removeMessage();
-
+    if (regex.test(email)){
+        alert("La direccion del email "+email+" es correcta.")
+        document.getElementById('formulario').submit()
     } else {
-        document.getElementById("emailBad").innerHTML+= mensajeAlert;
+        document.getElementById('emailBad').innerHTML += mensajeAlert; 
         removeMessage();
+      /* alert("La dirección de email es incorrecta."); */
     }
-});
-
-let mensajeAlert = '<p class="alert-message">Ingrese un Email valido</p>'
+  }
+      
+  let btnSubmit = document.getElementById('submitBtn')
+  
+  let mensajeAlert = '<p class="alert-message">Ingrese un Email valido</p>';
+  
+  
+  btnSubmit.addEventListener('click',function(event){
+  event.preventDefault();
+    
+    let email = document.getElementById('email').value
+  
+    validarEmail(email)
+      
+  })
